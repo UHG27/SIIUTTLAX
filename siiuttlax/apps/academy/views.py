c033a43c69fa61aea1119d8e39bcf2327abb5cec
lupisboni <guadaboni03@gmail.com>
2024-08-12 13:25:17 -0600

    correccion estudiante/profesor

--- siiuttlax/apps/academy/views.py
+++ siiuttlax/apps/academy/views.py
@@ -5,28 +5,10 @@ from .forms import ProfessorForm, StudentForm
 #create your views here.
 def create_Professor(request):
     if request.method == 'POST':
-        form =  ProfessorForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            first_name = form.cleaned_data['first_name']
-            password = form.cleaned_data['password']
-            enrollment = form.cleaned_data['enrollment']
-
-            Professor.objects.create_user(
-                username=username,
-                first_name=first_name,
-                password=password,
-                enrollment=enrollment
-            )
-            return redirect('academy:create_professor')
-        else:
-            print(form.errors)
-            
-    form = ProfessorForm()
-    return render(request
-                  ,'academy/create_professor.html',
+        form = ProfessorForm()
+    return render(request,
+                  'academy/create_professor.html',
                   {'form':form})
-
 
 def create_student(request):
     if request.method == 'POST':
